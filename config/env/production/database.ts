d535b1139c8ea0ec9e46a5c6241e0e6f151c3db3
FS <f.scholze@gmx.de>
2024-02-29 15:55:44 +0100

    Fix problem by using correct database again #2

--- config/env/production/database.ts
+++ config/env/production/database.ts
@@ -1,13 +1,19 @@
+const parse = require('pg-connection-string').parse
+const config = parse(process.env.DATABASE_URL)
+
 export default ({ env }) => ({
   connection: {
-    client: 'mysql',
+    client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', 'localhost'),
-      port: env.int('DATABASE_PORT', 3306),
-      database: env('DATABASE_NAME', 'dyrdomdej'),
-      user: env('DATABASE_USERNAME', 'dyrdoadmin'),
-      password: env('DATABASE_PASSWORD', 'mep$hoxagog+6jemkmap1l3v4c'),
-      ssl: env.bool('DATABASE_SSL', false)
-    }
+      host: config.host,
+      port: config.port,
+      database: config.database,
+      user: config.user,
+      password: config.password,
+      ssl: {
+        rejectUnauthorized: false
+      }
+    },
+    debug: false
   }
 })
